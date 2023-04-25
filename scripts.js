37c32e9e2c3a794d6af28fc6892fbabe55a9187d
ASHLEY <ach8448@nyu.edu>
2023-04-25 14:22:50 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,31 @@
+console.log("Hi");
+
+//1) get the reference of what you want to change
+let navImages = document.querySelectorAll(".cloud");
+
+let mainImage = document.querySelector(".airplane");
+
+//2) define the changes you want to make
+
+function greenCloud(){
+    mainImage.src = "images/clouds-green.jpg";
+    console.log("Hello");
+}
+
+function blueCloud(){
+    mainImage.src = "images/clouds-blue.jpg";
+}
+
+function redCloud(){
+    mainImage.src = "images/clouds-red.jpg";
+}
+
+function goBack(){
+    mainImage.src = "images/airplane.jpg";
+}
+//3) Call your function on a specific event
+
+navImages[0].onclick = blueCloud;
+navImages[1].onclick = greenCloud;
+navImages[2].onlick = redCloud;
+mainImage.onclick = goBack;
